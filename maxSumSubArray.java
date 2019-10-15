40850a507412b1e4e71b1f0a3df1fa575b7aa617
ettore34 <ettoremottolalares@gmail.com>
2019-10-14 21:51:12 -0400

    maxSumSubArray

---
+++ maxSumSubArray.java
@@ -0,0 +1,37 @@
+
+
+// find the max sum subArray of a afixed sized K 
+
+// Example input 
+// [4,2,1,7,8,1,2,8,1,0]
+
+public class maxSumSubArray
+{
+    public static int findmax(int[] arr, int k)
+    {
+        int maxValue = Integer.MIN_VALUE;
+        int currentRunningSum = 0;
+
+        for(int i = 0  ; i < arr.length; i++)
+        {
+            currentRunningSum += arr[i];
+
+            if(i >= k )
+            {
+                maxValue = Math.max(maxValue, currentRunningSum );
+                //keeps the window at the appropiate size
+                currentRunningSum -= arr[i - (k-1)];
+            }
+
+        }
+
+        return maxValue;
+    } 
+
+
+
+    public static void main (String[] args)
+    {
+        System.out.println( findmax(new int[] {4,2,1,7,8,1,2,8,1,0} , 3 ) );
+    }
+}
